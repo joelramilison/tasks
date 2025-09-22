c94b9f2b3b9f27a95c5edc978d75fe7369588780
Joel Ramilison <joel.ramilison@gmail.com>
2025-09-22 20:49:58 +0200

    remove file reading test

--- main.c
+++ main.c
@@ -6,12 +6,6 @@
 
 int main() {
 
-	struct TasksResult result = readTasks();
-	printf("Found %d tasks:\n\n", result.count);
-	for (int i = 0; i < result.count; i++) {
-		printf("%s (Open: %d)\n", result.tasks[i].title, result.tasks[i].open);
-	}
-	free(result.tasks);
 	
 
 	return 0;
